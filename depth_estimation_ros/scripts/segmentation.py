5ece4e8d1b1cb4247965cf5eb2aa68a3edf157ee
maliksyria <malik_m_z@hotmail.com>
2022-01-24 21:11:36 +0300

    Add files via upload

--- depth_estimation_ros/scripts/segmentation.py
+++ depth_estimation_ros/scripts/segmentation.py
@@ -16,18 +16,16 @@ class Segmenter(object):
     def __init__(self, topic_kf,topic_in, topic_out):
         self.cv_bridge = CvBridge()
         rospy.loginfo("Segmentation  " + topic_in +" output topic: " + topic_out)
-        #self.config_file = '/home/ami/slam_ws/SeMask-Segmentation/SeMask-FPN/configs/semask_swin/ade20k/semfpn_semask_swin_large_patch4_window12_640x640_80k_ade20k.py'
-        self.config_file = '/home/ami/slam_ws/SeMask-Segmentation/SeMask-FPN/configs/semask_swin/ade20k/semfpn_semask_swin_base_patch4_window12_512x512_80k_ade20k.py'
-        self.checkpoint_file = '/home/ami/slam_ws/SeMask-Segmentation/semask_base_fpn_ade20k.pth'
+        self.config_file = '/home/malik/Downloads/seg/mmsegmentation/configs/pspnet/pspnet_r50-d8_512x512_80k_ade20k.py'
+        self.checkpoint_file = '/home/malik/Downloads/seg/pspnet_r50-d8_512x512_80k_ade20k_20200615_014128-15a8b914.pth'
         self.model = init_segmentor(self.config_file, self.checkpoint_file, device='cuda:0')
         self.palette = np.array(self.model.PALETTE)
         #self.palette = np.array(self.palette)
         self.width=1280
         self.height=720
-        self.frame= None
-        self.stamp = None
         self.kernel=cv2.getStructuringElement(cv2.MORPH_RECT, (9, 9))
-
+        self.frame = None
+        self.stamp = None
         self.pub = rospy.Publisher(topic_out, Image, queue_size=1)
 
         #self.sub = rospy.Subscriber(topic_in, Image, self.image_rec, queue_size=1)
@@ -40,8 +38,8 @@ class Segmenter(object):
 
         self.idx=0
     def publish(self,result,frame_id,stamp_now):
-        result=cv2.cvtColor(result,cv2.COLOR_RGB2BGR)
-        img_msg = self.cv_bridge.cv2_to_imgmsg(result, encoding='rgb8')
+        result = cv2.cvtColor(result,cv2.COLOR_BGR2RGB)
+        img_msg = self.cv_bridge.cv2_to_imgmsg(result,encoding="rgb8")
         #img_msg.header = self.header
         img_msg.header.frame_id = frame_id
         img_msg.header.stamp = stamp_now
@@ -55,7 +53,7 @@ class Segmenter(object):
         #img = mmcv.imread(img)
         #img = img.copy()
         #h, w = img.shape[:2]
-        seg = result[0][0]
+        seg = result[0]
         color_seg = np.zeros((seg.shape[0], seg.shape[1], 3), dtype=np.uint8)
         for label, color in enumerate(self.palette):
             color_seg[seg == label, :] = color
@@ -63,14 +61,13 @@ class Segmenter(object):
         color_seg = mmcv.imresize(color_seg, (self.width, self.height), interpolation='nearest', backend='cv2')
         self.pred = color_seg.astype(np.uint8)
         #self.pred = cv2.morphologyEx(self.pred, cv2.MORPH_OPEN, self.kernel)
-        self.publish(self.pred,self.frame,self.stamp)
-        
+        self.publish(self.pred, self.frame, self.stamp)
     def image_rec(self, msg1,msg2):
         self.idx = self.idx + 1
         #rospy.loginfo("Received image " + str(self.idx))
         #self.header = Header(stamp=msg2.header.stamp)
-        self.frame= msg2.header.frame_id
-        self.stamp= msg2.header.stamp
+        self.frame = msg2.header.frame_id
+        self.stamp = msg2.header.stamp
         self.image_input = self.cv_bridge.imgmsg_to_cv2(msg1)
         self.segmant(self.image_input)
 
@@ -78,12 +75,8 @@ class Segmenter(object):
 if __name__ == '__main__':
     #argv = rospy.myargv(sys.argv)
     rospy.init_node("segmentation", anonymous=True,log_level=rospy.DEBUG)
-    #kf = rospy.get_param('~input_topic')
-    kf = '/vslam_stereo_inertial/KF_DebugImage'
-    #input_ori = rospy.get_param('~image_input_original_topic')
-    input_ori = '/zed2/zed_node/left/image_rect_color'
-    
-    #output_semantic = rospy.get_param('~segmentation_topic')
-    output_semantic = '/zed2/zed_node/semantics'
+    kf = rospy.get_param('~input_topic')
+    input_ori = rospy.get_param('~image_input_original_topic')
+    output_semantic = rospy.get_param('~segmentation_topic')
     seg = Segmenter(kf, input_ori, output_semantic)
     rospy.spin()
